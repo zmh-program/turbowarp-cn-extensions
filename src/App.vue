d1cb2b71be3657e3836d8bb524381ebe202178c9
Zhang Minghan <zmh13054618081@dingtalk.com>
2023-05-28 21:48:00 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-
 import ExtensionCard from "@/components/ExtensionCard.vue";
 </script>
 
--- src/components/ExtensionCard.vue
+++ src/components/ExtensionCard.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+//@ts-ignore
 import { ElNotification } from 'element-plus'
 
 const props = defineProps<{
